64e3d0ad4033dba1a6fc75533c11fe32bb9b4878
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-05-03 03:19:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const numero = 5;
+const segundoNumero = 10;
+
+console.log(numero + segundoNumero)
+
+const nome = 'peterson';
+const sobrenome = 'portella'
+const idade = 31;
+const altura = 1.83;
+const ano = 2024;
+const peso = 120;
+
+console.log(peso/(altura * altura))
+console.log(ano-idade)
+console.log(nome, sobrenome, 'tem', idade, 'e ele nasceu em', ano-idade )
